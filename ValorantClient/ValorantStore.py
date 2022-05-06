9d2afa261696428cee746ce0b87a3446e6585317
khalidsharmarke <ksharmarke972@gmail.com>
2022-05-06 17:21:14 -0400

    renamed V directory to ValorantClient, renamed ValorantClient to ValorantClientSession

---
+++ ValorantClient/ValorantStore.py
@@ -0,0 +1,20 @@
+import asyncio
+from ValorantClient import ValorantClientSession
+
+async def print_val_store():
+    client = ValorantClientSession()
+    await client.authenticate()
+    await client.get_store()
+    await client.close()
+
+
+
+if __name__ == '__main__':
+    try:
+        asyncio.get_event_loop().run_until_complete(print_val_store())
+    # intended errors
+    except SystemExit as e:
+        print(e)    
+    # unintended errors
+    except Exception as e:
+        print('event loop failed')
